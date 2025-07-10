6b47f9f51006e2a2c31b45b69cc1faa3f934fc06
Jim Albright <albrja@uw.edu>
2025-07-10 15:08:03 -0700

    Add class

---
+++ src/vivarium_csu_alzheimers/components/alzheimers.py
@@ -0,0 +1,6 @@
+from vivarium.framework.state_machine import State, TransientState
+from vivarium_public_health.disease import DiseaseModel, DiseaseState, SusceptibleState
+
+
+class Azheimers():
+    pass
