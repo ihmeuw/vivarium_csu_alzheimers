95b82a6f60768127eeeb4b600241d7041dbc41cf
Jim Albright <albrja@uw.edu>
2025-07-10 12:23:02 -0700

    Add live birth rate artifact key

--- src/vivarium_csu_alzheimers/constants/data_keys.py
+++ src/vivarium_csu_alzheimers/constants/data_keys.py
@@ -16,6 +16,7 @@ class __Population(NamedTuple):
     DEMOGRAPHY: str = "population.demographic_dimensions"
     TMRLE: str = "population.theoretical_minimum_risk_life_expectancy"
     ACMR: str = "cause.all_causes.cause_specific_mortality_rate"
+    LIVE_BIRTH_RATE: str = "covariate.live_births_by_sex.estimate"
 
     @property
     def name(self):
