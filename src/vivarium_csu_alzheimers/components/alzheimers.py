da89ae2e538df689abccc6102abbedf99bce5e47
Jim Albright <albrja@uw.edu>
2025-07-10 15:26:38 -0700

    Make model name named tuple

--- src/vivarium_csu_alzheimers/components/alzheimers.py
+++ src/vivarium_csu_alzheimers/components/alzheimers.py
@@ -1,6 +1,8 @@
 from vivarium.framework.state_machine import State, TransientState
 from vivarium_public_health.disease import DiseaseModel, DiseaseState, SusceptibleState
 
+from vivarium_csu_alzheimers.constants.models import __ALZHEIMERS_DISEASE_MODEL
+
 
 class Azheimers():
     pass
--- src/vivarium_csu_alzheimers/constants/models.py
+++ src/vivarium_csu_alzheimers/constants/models.py
@@ -1,14 +1,16 @@
+from typing import NamedTuple
+
 from vivarium_csu_alzheimers.constants import data_keys
 
 ###########################
 # Disease Model variables #
 ###########################
 
-# TODO input details of model states
-ALZHEIMERS_DISEASE = data_keys.ALZHEIMERS.name
-SUSCEPTIBLE_TO_ALZHEIMERS = f"susceptible_to_{ALZHEIMERS_DISEASE}"
-ALZHEIMERS_FIRST_STATE = "alzheimers_first_state"
-ALZHEIMERS_SECOND_STATE = "alzheimers_second_state"
-ALZHEIMERS_THIRD_STATE = "alzheimers_third_state"
-ALZHEIMERS_FOURTH_STATE = "alzheimers_fourth_state"
-ALZHEIMERS_FIFTH_STATE = "alzheimers_fifth_state"
+class __ALZHEIMERS_DISEASE_MODEL(NamedTuple):
+    ALZHEIMERS_DISEASE: str = data_keys.ALZHEIMERS.name
+    SUSCEPTIBLE_TO_ALZHEIMERS: str = f"susceptible_to_{ALZHEIMERS_DISEASE}"
+    ALZHEIMERS_FIRST_STATE: str = "alzheimers_first_state"
+    ALZHEIMERS_SECOND_STATE: str = "alzheimers_second_state"
+    ALZHEIMERS_THIRD_STATE: str = "alzheimers_third_state"
+    ALZHEIMERS_FOURTH_STATE: str = "alzheimers_fourth_state"
+    ALZHEIMERS_FIFTH_STATE: str = "alzheimers_fifth_state"
