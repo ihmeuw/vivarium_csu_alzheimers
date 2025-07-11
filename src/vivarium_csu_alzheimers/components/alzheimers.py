f77690955adfea47d130c84cc236aad719cfaa5c
Jim Albright <albrja@uw.edu>
2025-07-11 11:21:40 -0700

    Get alzheimers disease model working

--- src/vivarium_csu_alzheimers/components/alzheimers.py
+++ src/vivarium_csu_alzheimers/components/alzheimers.py
@@ -1,8 +1,16 @@
+import pandas as pd
 from vivarium import Component
 from vivarium.framework.state_machine import State, TransientState
 from vivarium_public_health.disease import DiseaseModel, DiseaseState, SusceptibleState
 
 from vivarium_csu_alzheimers.constants.models import ALZHEIMERS_DISEASE_MODEL
+
+# Constants for the Alzheimer's disease model
+# These values are placeholders and should be replaced with actual data from research later
+PREVALENCE = 1 / 6
+EXCESS_MORTALITY_RATE = 0.01
+DISABILITY_WEIGHT = 0.2
+TRANSITION_RATE = 0.75
 
 
 class Alzheimers(Component):
@@ -23,71 +31,55 @@ class Alzheimers(Component):
         susceptible = SusceptibleState(ALZHEIMERS_DISEASE_MODEL.SUSCEPTIBLE_TO_ALZHEIMERS)
         state_1 = DiseaseState(
             ALZHEIMERS_DISEASE_MODEL.ALZHEIMERS_FIRST_STATE,
-            # get_data_functions={
-            #     "prevalence": lambda *_: 1.0,
-            #     "disability_weight": lambda *_: 0.0,
-            #     "excess_mortality_rate": lambda *_: 0.0,
-            #     # Add a dummy dwell time so we can overwrite it later
-            #     "dwell_time": lambda *_: 0.0,
-            # },
+            prevalence=PREVALENCE,
+            disability_weight=DISABILITY_WEIGHT,
+            excess_mortality_rate=EXCESS_MORTALITY_RATE,
         )
         state_2 = DiseaseState(
             ALZHEIMERS_DISEASE_MODEL.ALZHEIMERS_SECOND_STATE,
-            # get_data_functions={
-            #     "prevalence": lambda *_: 0.0,
-            #     "disability_weight": lambda *_: 0.0,
-            #     "excess_mortality_rate": lambda *_: 0.0,
-            #     "dwell_time": lambda builder, cause: builder.time.step_size()(),
-            # },
+            prevalence=PREVALENCE,
+            disability_weight=DISABILITY_WEIGHT,
+            excess_mortality_rate=EXCESS_MORTALITY_RATE,
         )
         state_3 = DiseaseState(
             ALZHEIMERS_DISEASE_MODEL.ALZHEIMERS_THIRD_STATE,
-            # get_data_functions={
-            #     "prevalence": lambda *_: 0.0,
-            #     "disability_weight": lambda *_: 0.0,
-            #     "excess_mortality_rate": lambda *_: 0.0,
-            #     "dwell_time": lambda builder, cause: builder.time.step_size()(),
-            # },
+            prevalence=PREVALENCE,
+            disability_weight=DISABILITY_WEIGHT,
+            excess_mortality_rate=EXCESS_MORTALITY_RATE,
         )
         state_4 = DiseaseState(
             ALZHEIMERS_DISEASE_MODEL.ALZHEIMERS_FOURTH_STATE,
-            # get_data_functions={
-            #     "prevalence": lambda *_: 0.0,
-            #     "disability_weight": lambda *_: 0.0,
-            #     "excess_mortality_rate": lambda *_: 0.0,
-            #     "dwell_time": lambda builder, cause: builder.time.step_size()(),
-            # },
+            prevalence=PREVALENCE,
+            disability_weight=DISABILITY_WEIGHT,
+            excess_mortality_rate=EXCESS_MORTALITY_RATE,
         )
         state_5 = DiseaseState(
             ALZHEIMERS_DISEASE_MODEL.ALZHEIMERS_FIFTH_STATE,
-            # get_data_functions={
-            #     "prevalence": lambda *_: 0.0,
-            #     "disability_weight": lambda *_: 0.0,
-            #     "excess_mortality_rate": lambda *_: 0.0,
-            #     "dwell_time": lambda builder, cause: builder.time.step_size()(),
-            # },
+            prevalence=PREVALENCE,
+            disability_weight=DISABILITY_WEIGHT,
+            excess_mortality_rate=EXCESS_MORTALITY_RATE,
         )
 
         # Add transitions between states
         susceptible.add_transition(
             output_state=state_1,
-            probability_function=lambda *_: 0.75,
+            probability_function=lambda index: pd.Series(TRANSITION_RATE, index=index),
         )
         state_1.add_transition(
             output_state=state_2,
-            probability_function=lambda *_: 0.75,
+            probability_function=lambda index: pd.Series(TRANSITION_RATE, index=index),
         )
         state_2.add_transition(
             output_state=state_3,
-            probability_function=lambda *_: 0.75,
+            probability_function=lambda index: pd.Series(TRANSITION_RATE, index=index),
         )
         state_3.add_transition(
             output_state=state_4,
-            probability_function=lambda *_: 0.75,
+            probability_function=lambda index: pd.Series(TRANSITION_RATE, index=index),
         )
         state_4.add_transition(
             output_state=state_5,
-            probability_function=lambda *_: 0.75,
+            probability_function=lambda index: pd.Series(TRANSITION_RATE, index=index),
         )
         # TODO: do we need an "end" state?
 
