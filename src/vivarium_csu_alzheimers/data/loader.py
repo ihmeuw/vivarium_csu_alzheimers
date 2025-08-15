1999bd4e94404ad7130b914e527a338a2116c491
tylerdy <tylerdy@uw.edu>
2025-08-15 06:59:05 -0700

    WIP

--- src/vivarium_csu_alzheimers/data/loader.py
+++ src/vivarium_csu_alzheimers/data/loader.py
@@ -17,6 +17,7 @@ from typing import Any
 
 import numpy as np
 import pandas as pd
+import xarray as xr
 from gbd_mapping import causes, covariates, risk_factors
 from vivarium.framework.artifact import EntityKey
 from vivarium_gbd_access import gbd
@@ -76,9 +77,90 @@ def load_population_location(
     return location
 
 
+def table_from_nc(fname_dict, param, loc_id, loc_name, age_mapping):
+    ds = xr.open_dataset(
+        fname_dict[param],
+        engine="netcdf4",  # let xarray auto-detect; list here if you know it
+        decode_cf=True,  # handle CF-conventions (time units, etc.)
+    )
+
+    # Select relevant part of FHS dataset
+    var_name = param
+    if param == "births":
+        var_name = "population"
+    elif param in ["migration", "mortality"]:
+        var_name = "value"
+
+    pop_ts = ds[var_name].sel(
+        location_id=loc_id,
+    )
+
+    if param != "migration":
+        pop_ts = pop_ts.isel(scenario=0)
+
+    pop_ts = pop_ts.squeeze(drop=True)  # remove now-singleton dims
+
+    df = pop_ts.to_dataframe(name="value").reset_index()
+
+    # Transform to vivarium format
+    # 1. Convert location_id to location name
+    df["location"] = loc_name
+
+    # 2. Convert sex_id to sex names
+    sex_mapping = {
+        1: "Male",
+        2: "Female",
+    }
+    df["sex"] = df["sex_id"].map(sex_mapping)
+
+    # 3. Convert age_group_id to age intervals
+    if param != "births":
+        age_bins = age_mapping.set_index("age_group_id")
+        df["age_start"] = np.round(df["age_group_id"].map(age_bins["age_start"]), 3)
+        df["age_end"] = np.round(df["age_group_id"].map(age_bins["age_end"]), 3)
+        age_cols = ["age_start", "age_end"]
+    else:
+        age_cols = []
+
+    # 4. Convert year_id to year intervals
+    df["year_start"] = df["year_id"].map(int)
+    df["year_end"] = df["year_id"].map(int) + 1
+
+    # 5. Set index and unstack to get draw columns
+    index_cols = (
+        [
+            "location",
+            "sex",
+        ]
+        + age_cols
+        + ["year_start", "year_end", "draw"]
+    )
+    df_indexed = df.dropna(subset=index_cols).set_index(index_cols)
+
+    df_wide = df_indexed["value"].unstack(level="draw")
+
+    # 6. Rename columns to draw_x format
+    df_wide.columns = [f"draw_{col}" for col in df_wide.columns]
+
+    return df_wide
+
+
+def get_fname_dict():
+    return {
+        "population": "/mnt/share/forecasting/data/9/future/population/20240320_daly_capstone_resubmission_squeeze_soft_round_shifted_hiv_shocks_covid_all_who_reagg/population_agg.nc",
+        #     'births': '/mnt/share/forecasting/data/9/future/live_births/20231204_ref/live_births.nc',
+        #     'deaths': '/snfs1/Project/forecasting/results/7/future/death/20240320_daly_capstone_resubmission_squeeze_soft_round_shifted_hiv_shocks_covid_all_who_reagg_num/_all.nc',
+        "mortality": "/snfs1/Project/forecasting/results/7/future/death/20240320_daly_capstone_resubmission_squeeze_soft_round_shifted_hiv_shocks_covid_all_who_reagg/_all.nc",
+        #     'migration': '/mnt/share/forecasting/data/6/future/migration/20230605_loc_intercept_shocks_pg_21LOCS_ATTENUATED/migration.nc',
+    }
+
+
 def load_population_structure(
     key: str, location: str, years: int | str | list[int] | None = None
 ) -> pd.DataFrame:
+    
+
+
     return interface.get_population_structure(location, years)
 
 
@@ -105,7 +187,9 @@ def load_standard_data(
 ) -> pd.DataFrame:
     key = EntityKey(key)
     entity = get_entity(key)
-    return interface.get_measure(entity, key.measure, location, years).droplevel("location")
+    return interface.get_measure(entity, key.measure, location, years).droplevel(
+        "location"
+    )
 
 
 def load_metadata(key: str, location: str, years: int | str | list[int] | None = None):
@@ -159,8 +243,12 @@ def _load_em_from_meid(location, meid, measure):
     data = data.filter(vi_globals.DEMOGRAPHIC_COLUMNS + vi_globals.DRAW_COLUMNS)
     data = vi_utils.reshape(data)
     data = vi_utils.scrub_gbd_conventions(data, location)
-    data = vi_utils.split_interval(data, interval_column="age", split_column_prefix="age")
-    data = vi_utils.split_interval(data, interval_column="year", split_column_prefix="year")
+    data = vi_utils.split_interval(
+        data, interval_column="age", split_column_prefix="age"
+    )
+    data = vi_utils.split_interval(
+        data, interval_column="year", split_column_prefix="year"
+    )
     return vi_utils.sort_hierarchical_data(data).droplevel("location")
 
 
@@ -173,7 +261,8 @@ def load_alzheimers_prevalence(
     """Need to return the "opposite" prevalence key we are trying to write to the artifact.
     Meaning, we want the prevalence scale factor, to be the normal GBD prevalence of Alzheimers to properly
     scale the population and "fertility" components of the model. We want the Alzheimers SI model to really
-    only be an I model, so we will set the prevalence to 1, so all simulants are created with the disease."""
+    only be an I model, so we will set the prevalence to 1, so all simulants are created with the disease.
+    """
     prevalence = load_standard_data(data_keys.ALZHEIMERS.PREVALENCE, location, years)
     if key == data_keys.ALZHEIMERS.PREVALENCE_SCALE_FACTOR:
         return prevalence
