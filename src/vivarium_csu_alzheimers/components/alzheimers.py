c1d382988646ece64f2205e774a83a798a8d5571
Jim Albright <albrja@uw.edu>
2025-09-08 16:48:15 -0700

    Update data for disease model and add subclasses

--- src/vivarium_csu_alzheimers/components/alzheimers.py
+++ src/vivarium_csu_alzheimers/components/alzheimers.py
@@ -1,18 +1,41 @@
+from typing import Callable
 import pandas as pd
 from vivarium import Component
-from vivarium.framework.state_machine import State, TransientState
-from vivarium_public_health.disease import DiseaseModel, DiseaseState, SusceptibleState
+from vivarium.framework.engine import Builder
+from vivarium.framework.state_machine import Trigger
+from vivarium.types import DataInput
+from vivarium_public_health.disease import BaseDiseaseState, DiseaseModel, DiseaseState, RateTransition
 
-from vivarium_csu_alzheimers.constants.data_keys import ALZHEIMERS
+from vivarium_csu_alzheimers.constants.data_keys import ALZHEIMERS, POPULATION
+from vivarium_csu_alzheimers.constants import data_values
 from vivarium_csu_alzheimers.constants.models import ALZHEIMERS_DISEASE_MODEL
 
-# Constants for the Alzheimer's disease model
-# These values are placeholders and should be replaced with actual data from research later
-PREVALENCE = 1 / 6
-EXCESS_MORTALITY_RATE = 0.035
-DISABILITY_WEIGHT = 0.2
-TRANSITION_RATE = 0.25
 
+class BBBMTransitionRate(RateTransition):
+    def _probability(self, index: pd.Index) -> pd.Series:
+        super()._probability(index)
+
+class BBBMDiseaseState(DiseaseState):
+    def add_rate_transition(
+        self,
+        output: BaseDiseaseState,
+        get_data_functions: dict[str, Callable] = None,
+        triggered=Trigger.NOT_TRIGGERED,
+        transition_rate: DataInput | None = None,
+        rate_type: str = "transition_rate",
+    ) -> RateTransition:
+        """Method to instantiate BBBMTransitionRate and add it to the disease state."""
+        transition = BBBMTransitionRate(
+            input_state=self,
+            output_state=output,
+            get_data_functions=get_data_functions,
+            triggered=triggered,
+            transition_rate=transition_rate,
+            rate_type=rate_type,
+        )
+        self.add_transition(transition)
+        return transition
+    
 
 class Alzheimers(Component):
     """A class to hold the Alzheimer's disease model. This class includes the
@@ -33,29 +56,27 @@ class Alzheimers(Component):
         self.bbbm_prevalence = builder.data.load(ALZHEIMERS.BBBM_CONDITIONAL_PREVALANCE)
         self.mci_prevalence = builder.data.load(ALZHEIMERS.MCI_CONDITIONAL_PREVALENCE)
         self.alzheimers_prevalence = builder.data.load(ALZHEIMERS.PREVALENCE)
-        # TODO: bbbm disability weight
         self.mci_disability_weight = builder.data.load(ALZHEIMERS.MCI_DISABILITY_WEIGHT)
-        self.alzheimers_disability_weight = builder.data.load(ALZHEIMERS.DISABILIITY_WEIGHT)
+        self.alzheimers_disability_weight = builder.data.load(ALZHEIMERS.DISABILITY_WEIGHT)
         self.alzheimers_emr = builder.data.load(ALZHEIMERS.EMR)
-        # TODO: load bbbm excess mortality rate = 0.0
-        # TODO: load mci excess mortality rate = 0.0
         # TODO: load transition rates
         self.csmr = builder.data.load(ALZHEIMERS.CSMR)
+        self.mci_to_alzheimers_rate = self.get_mci_to_alzheimers_rate(builder)
 
     def _create_disease_model(self):
-        bbbm_state = DiseaseState(
+        bbbm_state = BBBMDiseaseState(
             ALZHEIMERS_DISEASE_MODEL.BBBM_STATE,
             allow_self_transition=True,
             prevalence=self.bbbm_prevalence,
-            disability_weight=DISABILITY_WEIGHT,
-            excess_mortality_rate=0.0,
+            disability_weight=data_values.DW_BBBM,
+            excess_mortality_rate=data_values.EMR_BBBM,
         )
         mci_state = DiseaseState(
             ALZHEIMERS_DISEASE_MODEL.MCI_STATE,
             allow_self_transition=True,
             prevalence=self.mci_prevalence,
             disability_weight=self.mci_disability_weight,
-            excess_mortality_rate=0.0,
+            excess_mortality_rate=data_values.EMC_MCI,
         )
         alzheimers_state = DiseaseState(
             ALZHEIMERS_DISEASE_MODEL.ALZHEIMERS_DISEASE_STATE,
@@ -65,15 +86,16 @@ class Alzheimers(Component):
         )
 
         # Add transitions between states
+        # TODO: write Transition (RateTranstion)
         bbbm_state.add_rate_transition(
             output=mci_state,
-            # TODO: update to get_data_functions
-            transition_rate=TRANSITION_RATE,
+            # TODO: update to use hazard rate calculation
+            transition_rate=0.25,  # placeholder value
         )
         mci_state.add_rate_transition(
             output=alzheimers_state,
-            # TODO: update to get_data_functions
-            transition_rate=TRANSITION_RATE,
+            # TODO: update to calculate rate
+            transition_rate=self.mci_to_alzheimers_rate,
         )
 
         return DiseaseModel(
@@ -81,3 +103,11 @@ class Alzheimers(Component):
             initial_state=bbbm_state,
             states=[bbbm_state, mci_state, alzheimers_state],
         )
+
+    def get_mci_to_alzheimers_rate(self, builder: Builder) -> pd.DataFrame:
+        # 1/dealth_mci - (acmr - csmr_c543 + emr_mci)
+        acmr = builder.data.load(POPULATION.ACMR)
+        alz_csmr = builder.data.load(ALZHEIMERS.CSMR)
+        m_mci = acmr - alz_csmr + data_values.EMC_MCI
+        transition_rate = 1 / data_values.MCI_AVG_DURATION - m_mci
+        return transition_rate
--- src/vivarium_csu_alzheimers/components/population.py
+++ src/vivarium_csu_alzheimers/components/population.py
@@ -103,6 +103,8 @@ class AlzheimersIncidence(Component):
         # NOTE: All three of these methods are capping the upper age bound at 100
         self.bbbm_incidence_counts = self.load_bbbm_incidence_counts(builder)
         self.pop_structure = self.load_population_structure(builder)
+        # TODO: load from artifact when ready
+        self.preclinical_deaths = 0.0
         prevalence = self.load_prevalence(builder)
         start_year = builder.configuration.time.start.year
         sub_pop = self.pop_structure.loc[
@@ -145,7 +147,8 @@ class AlzheimersIncidence(Component):
             self.bbbm_incidence_counts.index.get_level_values("year_start") == query_year
         ]
         incident_cases = incident_cases.droplevel(["year_start", "year_end"])
-        mean_incident_cases = incident_cases * self.model_scale
+        # TODO: this needs to be updated
+        mean_incident_cases = self.model_scale * (incident_cases + self.preclinical_deaths)
         simulants_to_add = pd.Series(0, index=mean_incident_cases.index)
 
         # Determine number of simulants to add for each demographic group
