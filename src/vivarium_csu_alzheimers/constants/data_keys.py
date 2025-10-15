d595b858c69981faa14aa1710be2520877d194b7
Jim Albright <albrja@uw.edu>
2025-10-15 11:30:35 -0700

    Remove keys

--- src/vivarium_csu_alzheimers/constants/data_keys.py
+++ src/vivarium_csu_alzheimers/constants/data_keys.py
@@ -9,13 +9,13 @@ METADATA_LOCATIONS = "metadata.locations"
 
 class __Population(NamedTuple):
     LOCATION: str = "population.location"
-    STRUCTURE: str = "population.structure"
+    # STRUCTURE: str = "population.structure"
     AGE_BINS: str = "population.age_bins"
-    DEMOGRAPHY: str = "population.demographic_dimensions"
-    TMRLE: str = "population.theoretical_minimum_risk_life_expectancy"
-    ACMR: str = "cause.all_causes.cause_specific_mortality_rate"
+    # DEMOGRAPHY: str = "population.demographic_dimensions"
+    # TMRLE: str = "population.theoretical_minimum_risk_life_expectancy"
+    # ACMR: str = "cause.all_causes.cause_specific_mortality_rate"
     LIVE_BIRTH_RATE: str = "covariate.live_births_by_sex.estimate"
-    SCALING_FACTOR: str = "population.scaling_factor"
+    # SCALING_FACTOR: str = "population.scaling_factor"
 
     @property
     def name(self):
@@ -30,20 +30,20 @@ POPULATION = __Population()
 
 
 class __Alzheimers(NamedTuple):
-    PREVALENCE: str = "cause.alzheimers.prevalence"
-    BBBM_CONDITIONAL_PREVALANCE: str = "cause.alzheimers.bbbm_conditional_prevalence"
-    MCI_CONDITIONAL_PREVALENCE: str = "cause.alzheimers.mci_conditional_prevalence"
-    MCI_TO_DEMENTIA_TRANSITION_RATE: str = "cause.alzheimers.mci_to_dementia_transition_rate"
-    SUSCEPTIBLE_TO_BBBM_TRANSITION_COUNT: str = (
-        "cause.alzheimers.susceptible_to_bbbm_transition_count"
-    )
-    # BBBM to MCI transition rate caluclated during sim using mci_hazard.py and time in state
-    INCIDENCE_RATE_TOTAL_POPULATION: str = "cause.alzheimers.population_incidence_rate"
+    # PREVALENCE: str = "cause.alzheimers.prevalence"
+    # BBBM_CONDITIONAL_PREVALANCE: str = "cause.alzheimers.bbbm_conditional_prevalence"
+    # MCI_CONDITIONAL_PREVALENCE: str = "cause.alzheimers.mci_conditional_prevalence"
+    # MCI_TO_DEMENTIA_TRANSITION_RATE: str = "cause.alzheimers.mci_to_dementia_transition_rate"
+    # SUSCEPTIBLE_TO_BBBM_TRANSITION_COUNT: str = (
+    #     "cause.alzheimers.susceptible_to_bbbm_transition_count"
+    # )
+    # # BBBM to MCI transition rate caluclated during sim using mci_hazard.py and time in state
+    # INCIDENCE_RATE_TOTAL_POPULATION: str = "cause.alzheimers.population_incidence_rate"
     CSMR: str = "cause.alzheimers_disease_and_other_dementias.cause_specific_mortality_rate"
     EMR: str = "cause.alzheimers_disease_and_other_dementias.excess_mortality_rate"
     DISABILITY_WEIGHT: str = "cause.alzheimers_disease_and_other_dementias.disability_weight"
-    MCI_DISABILITY_WEIGHT: str = "cause.alzheimers.mci_disability_weight"
-    RESTRICTIONS: str = "cause.alzheimers_disease_and_other_dementias.restrictions"
+    # MCI_DISABILITY_WEIGHT: str = "cause.alzheimers.mci_disability_weight"
+    # RESTRICTIONS: str = "cause.alzheimers_disease_and_other_dementias.restrictions"
 
     @property
     def name(self):
