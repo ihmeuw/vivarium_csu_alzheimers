a997a8210c6b9bee0b602ac954150ff1fc519c46
Steve Bachmeier <sbachmei@uw.edu>
2025-10-21 16:25:17 -0600

    stop double-multiplying by time step

--- src/vivarium_csu_alzheimers/data/mci_hazard.py
+++ src/vivarium_csu_alzheimers/data/mci_hazard.py
@@ -1,11 +1,6 @@
 # copied from Nathaniel's notebook
 import numpy as np
 
-from vivarium_csu_alzheimers.constants.data_values import BBBM_HAZARD_DIST
-
 
 def hazard(t, dist):
     return np.exp(dist.logpdf(t) - dist.logsf(t))
-
-
-gamma_hazard = lambda t: hazard(t, BBBM_HAZARD_DIST)
