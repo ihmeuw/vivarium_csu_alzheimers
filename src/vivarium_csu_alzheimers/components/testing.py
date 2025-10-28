f61fd60c0ade08d97703963d69dd8cd0a561097b
Jim Albright <albrja@uw.edu>
2025-10-28 15:48:12 -0700

    Use testing rate for testing history on initialization

--- src/vivarium_csu_alzheimers/components/testing.py
+++ src/vivarium_csu_alzheimers/components/testing.py
@@ -80,12 +80,15 @@ class Testing(Component):
 
         # Update to reflect history
         event_time = pop_data.creation_time + get_timedelta_from_step_size(self.step_size)
-        bbbm_eligible_mask = self._get_bbbm_eligible_simulants(pop, event_time)
-        pop[COLUMNS.BBBM_TEST_DATE] = self._generate_bbbm_testing_history(
-            pop, bbbm_eligible_mask, event_time
-        )
-
         if self.scenario.bbbm_testing:
+            bbbm_eligible_mask = self._get_bbbm_eligible_simulants(pop, event_time)
+            testing_rate = self._get_bbbm_testing_rate(event_time)
+            tested_mask = bbbm_eligible_mask & (
+                pop[COLUMNS.TESTING_PROPENSITY] < testing_rate
+            )
+            pop[COLUMNS.BBBM_TEST_DATE] = self._generate_bbbm_testing_history(
+                pop, tested_mask, event_time
+            )
             # All previous BBBM tests are negative
             pop.loc[bbbm_eligible_mask, COLUMNS.BBBM_TEST_RESULT] = BBBM_TEST_RESULTS.NEGATIVE
             pop.loc[bbbm_eligible_mask, COLUMNS.BBBM_TEST_EVER_ELIGIBLE] = True
@@ -217,6 +220,7 @@ class Testing(Component):
             time_of_event + get_timedelta_from_step_size(self.step_size, time_step)
             for time_step in range(0, -6, -1)
         ]
+        # Calculate test results
         test_dates.loc[eligible_sims] = self.randomness.choice(
             index=simulants[eligible_sims].index,
             choices=test_date_options,
