200a27296600ac5865b5dedcb053fdf9c16572db
Jim Albright <albrja@uw.edu>
2026-01-30 10:15:13 -0800

    Update ramp start and artifact paths

--- src/vivarium_csu_alzheimers/constants/data_values.py
+++ src/vivarium_csu_alzheimers/constants/data_values.py
@@ -169,15 +169,16 @@ BBBM_TEST_RESULTS = __BBBMTestResults()
 
 # bbbm testing rates are piecewise-linear starting at 2030 and maxing out in 2045
 BBBM_TESTING_RATES = [
-    (pd.Timestamp("2027-01-01"), 0.1),  # step increase from 0 in 2030
+    (pd.Timestamp("2030-01-01"), 0.1),  # step increase from 0 in 2030
     (pd.Timestamp("2035-01-01"), 0.2),
     (pd.Timestamp("2040-01-01"), 0.4),
     (pd.Timestamp("2045-01-01"), 0.6),  # plateaus from here on out
 ]
 BBBM_TESTING_START_DATE = BBBM_TESTING_RATES[0][0]
 
+# TODO: update time start of ramp with test model updates
 TREATMENT_PROBS_RAMP = [
-    (pd.Timestamp("2027-01-01"), 0.0),
+    (pd.Timestamp("2030-01-01"), 0.0),
     (pd.Timestamp("2035-01-01") + pd.Timedelta(days=182), 0.3),
     (pd.Timestamp("2100-01-01"), 0.8),
 ]
